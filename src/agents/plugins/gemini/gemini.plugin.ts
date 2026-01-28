47520cf0b966771d73e76c6cf140da962a2b7801
Dmitriy Stoyanov <dmitry_stoyanov@epam.com>
2026-01-28 20:10:37 +0300

    fix: wrong gemini settings json (#107)

--- src/agents/plugins/gemini/gemini.plugin.ts
+++ src/agents/plugins/gemini/gemini.plugin.ts
@@ -124,9 +124,6 @@ export const GeminiPluginMetadata: AgentMetadata = {
           },
           tools: {
             enableHooks: true
-          },
-          hooks: {
-            enabled: true
           }
         }
       );
